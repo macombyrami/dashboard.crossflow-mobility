8e33b8d978d0529e123855fb96d16790e16600dd
macombyrami <macom.rami@gmail.com>
2026-04-03 08:38:28 +0200

    Update uiStore.ts

--- src/store/uiStore.ts
+++ src/store/uiStore.ts
@@ -1,13 +1,27 @@
 import { create } from 'zustand'
 
 interface UIStore {
-  isSidebarOpen: boolean
-  setSidebarOpen: (open: boolean) => void
-  toggleSidebar: () => void
+  isSidebarOpen:        boolean
+  isNotificationOpen:   boolean
+  isCommandPaletteOpen: boolean
+  
+  setSidebarOpen:        (open: boolean) => void
+  setNotificationOpen:   (open: boolean) => void
+  setCommandPaletteOpen: (open: boolean) => void
+  
+  toggleSidebar:         () => void
+  toggleNotification:    () => void
 }
 
 export const useUIStore = create<UIStore>()((set) => ({
-  isSidebarOpen: false,
-  setSidebarOpen: (open) => set({ isSidebarOpen: open }),
-  toggleSidebar: () => set((s) => ({ isSidebarOpen: !s.isSidebarOpen })),
+  isSidebarOpen:        false,
+  isNotificationOpen:   false,
+  isCommandPaletteOpen: false,
+
+  setSidebarOpen:        (open) => set({ isSidebarOpen: open }),
+  setNotificationOpen:   (open) => set({ isNotificationOpen: open }),
+  setCommandPaletteOpen: (open) => set({ isCommandPaletteOpen: open }),
+
+  toggleSidebar:         () => set((s) => ({ isSidebarOpen: !s.isSidebarOpen })),
+  toggleNotification:    () => set((s) => ({ isNotificationOpen: !s.isNotificationOpen })),
 }))
